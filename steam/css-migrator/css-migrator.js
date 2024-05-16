ba28e486b44a289fed9b92b82325ab8d0c1aa8ac
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2024-05-16 15:13:44 +0200

    Added WIP css-migrator.

---
+++ steam/css-migrator/css-migrator.js
@@ -0,0 +1,69 @@
+const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
+
+async function run() {
+  const [mappingUrl, sourcePath, outputPath] = process.argv.slice(2);
+
+  if (!mappingUrl || !sourcePath || !outputPath) {
+    console.error(
+      'Please provide three arguments: mapping_url, source_path, output_path'
+    );
+    return;
+  }
+
+  try {
+    const mappingData = await fetchMappingData(mappingUrl);
+    const files = await fs.readdir(sourcePath);
+
+    for (const fileName of files) {
+      if (fileName.endsWith('.css')) {
+        await processFile(mappingData, sourcePath, outputPath, fileName);
+      }
+    }
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+async function fetchMappingData(mappingUrl) {
+  try {
+    const response = await axios.get(mappingUrl);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching mapping data: ${error}`);
+  }
+}
+
+async function processFile(mappingData, sourcePath, outputPath, fileName) {
+  const sourceFile = path.join(sourcePath, fileName);
+  const outputFile = path.join(outputPath, fileName);
+
+  try {
+    let content = await fs.readFile(sourceFile, 'utf8');
+
+    for (const [_, children] of Object.entries(mappingData)) {
+      const replacement = `.${children[children.length - 1]}`;
+
+      children.forEach((child) => {
+        const pattern = `\\[class(?:\\*|\\^)?=(?:'|")${escapeRegex(
+          child
+        )}(?:'|")\\]`;
+        content = content.replace(new RegExp(pattern, 'g'), replacement);
+      });
+    }
+
+    await fs.writeFile(outputFile, content, 'utf8');
+    console.log(
+      `Replaced child strings with parent keys in ${sourceFile} (written to ${outputFile})`
+    );
+  } catch (error) {
+    console.error(`Error processing file: ${sourceFile} - ${error}`);
+  }
+}
+
+function escapeRegex(string) {
+  return string.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+run();
---
+++ steam/css-migrator/readme.md
@@ -0,0 +1,8 @@
+## Requirements
+
+1. have `node` installed
+2. run `npm install`
+
+## Usage
+
+`node css-migrator.js https://api.deckthemes.com/beta.json ../custom-css/src/ ../custom-css/`
