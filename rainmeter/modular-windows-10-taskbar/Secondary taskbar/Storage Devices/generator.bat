1368e3c2aaf51d3cb6d48cbebbf5b50b757c4444
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-11-14 15:31:23 +0100

    Updated license.

--- rainmeter/modular-windows-10-taskbar/Secondary taskbar/Storage Devices/generator.bat
+++ rainmeter/modular-windows-10-taskbar/Secondary taskbar/Storage Devices/generator.bat
@@ -5,7 +5,7 @@ for %%p in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
 		echo Version = 1.0
 		echo Description = Displays the current usage of the specified HDD
 		echo Author = MetalTxus
-		echo License = Creative Commons BY-NC 3.0
+		echo License = CC BY-NC 4.0
 		echo.
 		echo [Variables]
 		echo DriveLetter = %%p:\
